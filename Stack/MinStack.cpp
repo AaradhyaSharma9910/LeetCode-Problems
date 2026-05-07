71907bda60432d95af444c41c2614d4affb43db7
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-05-07 21:52:13 +0530

    added the min stack

---
+++ Stack/MinStack.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+class MinStack {
+public:
+stack<pair<int,int>> st;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(st.empty())
+        {
+            st.push({val,val});
+        }
+        else{
+            int currMin = min(val,st.top().second);
+            st.push({val, currMin});
+        }
+    }
+    
+    void pop() {
+        st.pop();
+        
+    }
+    
+    int top() {
+        return st.top().first;
+        
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
+
