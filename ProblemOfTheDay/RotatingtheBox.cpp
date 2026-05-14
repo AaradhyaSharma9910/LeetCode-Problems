e2e3878c05a8a7b953ba6ee8dba28464b3b62895
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-05-14 20:04:15 +0530

    updated code

--- ProblemOfTheDay/RotatingtheBox.cpp
+++ ProblemOfTheDay/RotatingtheBox.cpp
@@ -4,34 +4,45 @@ using namespace std;
 class Solution {
 public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
+
         int m = boxGrid.size();
         int n = boxGrid[0].size();
-        // applying gravity in each row
-        for(int i=0;i<m;i++)
-        {
-            int emptyPos = n-1;
-            for(int j=n-1 ;j>=0;j--)
-            {
-                //obstacle
-                if(boxGrid[i][j]=='*'){
-                    emptyPos = j-1;
+
+        // Apply gravity
+        for(int i = 0; i < m; i++) {
+
+            int emptyPos = n - 1;
+
+            for(int j = n - 1; j >= 0; j--) {
+
+                // obstacle
+                if(boxGrid[i][j] == '*') {
+                    emptyPos = j - 1;
                 }
-                //stone
-                else if(boxGrid[i][j]=='#'){
-                    swap(boxGrid[i][j],boxGrid[i][emptyPos]);
+
+                // stone
+                else if(boxGrid[i][j] == '#') {
+
+                    if(j != emptyPos) {
+                        boxGrid[i][emptyPos] = '#';
+                        boxGrid[i][j] = '.';
+                    }
+
                     emptyPos--;
                 }
             }
         }
-        // rotate matrix clockwise
-        vector<vector<char>> ans(n, vector<char>(m));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++)
-            {
-                ans[j][m-1-i] = boxGrid[i][j];
 
+        // rotate clockwise
+        vector<vector<char>> ans(n, vector<char>(m));
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+
+                ans[j][m - 1 - i] = boxGrid[i][j];
             }
         }
+
         return ans;
     }
 };
