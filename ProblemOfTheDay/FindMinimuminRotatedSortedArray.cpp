b478cdf87241f331558a9cc677ebde60593c14f8
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-05-19 21:52:56 +0530

    added problem of the day of 15th may

---
+++ ProblemOfTheDay/FindMinimuminRotatedSortedArray.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        return *partition_point(nums.begin(), nums.end(), [&](int n) { return n > nums.back(); });
+    }
+};
