74ccd7753414eae4e074f23ad6abb0e6cfaf5460
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-07-16 13:32:43 +0530

    final code

--- ProblemOfTheDay/SumOfGcdOfFormedPairs.cpp
+++ ProblemOfTheDay/SumOfGcdOfFormedPairs.cpp
@@ -6,23 +6,19 @@ public:
     long long gcdSum(vector<int>& A) {
         int mx = 0;
 
-        // Compute prefix GCDs with the maximum element seen so far
         for (auto &n : A) {
-            mx = std::max(mx, n);
-            n = std::gcd(n, mx);
+            mx = max(mx, n);
+            n = __gcd(n, mx);
         }
 
-        // Sort the array
         sort(A.begin(), A.end());
 
         long long res = 0;
-
-        // Sum of gcd of formed pairs
         int i = 0;
         int j = (int)A.size() - 1;
 
         while (i < j) {
-            res += std::gcd(A[i], A[j]);
+            res += __gcd(A[i], A[j]);
             i++;
             j--;
         }
